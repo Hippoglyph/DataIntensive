3e7b320a7a8025d3258b20e66ffd7bc4693cc18b
Hippoglyph <jonathanrinnarv@hotmail.com>
2018-10-15 17:07:54 +0200

    Started project. Tweets comming in

---
+++ project/tweetClassifier.scala
@@ -0,0 +1,48 @@
+import org.apache.spark.streaming.twitter._
+import org.apache.spark._
+import org.apache.spark.streaming._
+import twitter4j.auth.OAuthAuthorization
+import twitter4j.conf.ConfigurationBuilder
+import org.apache.spark.sql._
+import Constants._
+//import org.apache.spark.streaming.StreamingContext._
+
+
+object TweetClassifier {
+	def main(args: Array[String]) {
+
+		val conf = new SparkConf().setAppName("BFFs sick stream").setMaster("local[2]")
+		val ssc = new StreamingContext(conf, Seconds(10))
+		ssc.checkpoint("checkpoint")
+
+		val builder = new ConfigurationBuilder()
+	    builder.setOAuthConsumerKey(Constants.consumerKey())
+	    builder.setOAuthConsumerSecret(Constants.consumerSecret())
+	    builder.setOAuthAccessToken(Constants.accessToken())
+	    builder.setOAuthAccessTokenSecret(Constants.accessSecret())
+	    builder.setTweetModeExtended(true)
+	    val configuration = builder.build()
+
+		val stream = TwitterUtils.createStream(ssc, Some(new OAuthAuthorization(configuration)))
+
+		val results = stream.foreachRDD(rdd => {
+			val spark = SparkSession.builder.config(rdd.sparkContext.getConf).getOrCreate()
+			import spark.implicits._
+
+			val myTweets = rdd.filter(tweet => tweet.getPlace() != null).filter(tweet => tweet.getPlace().getCountry() != null).filter(tweet => tweet.getLang() != null && tweet.getLang() == "en")
+			if (!myTweets.isEmpty)
+				myTweets.map(tweet => {
+					val text  = {
+						if (tweet.getRetweetedStatus() != null)
+							tweet.getRetweetedStatus().getText()
+						else
+							tweet.getText()
+					}
+					(tweet.getPlace().getCountry, text)
+				}).toDF("Country", "Tweet").coalesce(1).write.partitionBy("Country").mode(SaveMode.Append).json("tweets")
+		})
+
+		ssc.start()
+    	ssc.awaitTermination()
+	}
+}
