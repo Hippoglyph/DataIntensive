f746b8df6a970f2f608daae48c4c4fd6cbc4516d
Hippoglyph <jonathanrinnarv@hotmail.com>
2018-10-20 15:20:25 +0200

    Almost work

--- project/Trainer.scala
+++ project/Trainer.scala
@@ -7,7 +7,13 @@ import org.apache.spark.sql._
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
 import Constants._
+import Word2vec._
 //import org.apache.spark.streaming.StreamingContext._
+
+import org.apache.spark.sql.cassandra._
+import com.datastax.spark.connector._
+import com.datastax.driver.core.{Session, Cluster, Host, Metadata}
+import com.datastax.spark.connector.streaming._
 
 
 object Trainer {
@@ -23,6 +29,26 @@ object Trainer {
 		TwitterUtils.createStream(ssc, Some(new OAuthAuthorization(configuration)))
 	}
 
+	def createCassandra(conf: SparkConf) = {
+		val sparkn = SparkSession.builder.config(conf).config("spark.cassandra.connection.host", "127.0.0.1").getOrCreate()
+		val sc = sparkn.sparkContext
+		import sparkn.implicits._
+
+		val cluster = Cluster.builder().addContactPoint("127.0.0.1").build()
+	    val session = cluster.connect()
+	    session.execute("CREATE KEYSPACE IF NOT EXISTS project WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };")
+	    session.execute("CREATE TABLE IF NOT EXISTS project.words (word text PRIMARY KEY, id int, vector list<double>);")
+	    session
+	}
+
+	def writeToCassandra(wordData: scala.collection.mutable.Map[String, Int], sc: SparkContext){
+		val mapRDD = sc.parallelize((wordData.map{case (k, v) => (k, v, List(2, 2))}).toSeq)
+		mapRDD.saveToCassandra("project", "words", SomeColumns("word", "id", "vector"))
+		/*for((k, v) <- wordData){
+			session.execute("INSERT INTO project.words (word, id, vector) VALUES ('"+k+"',"+v+",[2,2]);")
+		}*/
+	}
+
 	def main(args: Array[String]) {
 
 		Logger.getLogger("org").setLevel(Level.OFF)
@@ -32,30 +58,54 @@ object Trainer {
 		val sc = new SparkContext(conf)
 		val ssc = new StreamingContext(sc, Seconds(10))
 		ssc.checkpoint("checkpoint")
-
-		val word2vec = new Word2vec(conf)
+		val session = createCassandra(conf)
 
 		val stream = getStream(ssc)
+
+		var wordData = Word2vec.getInitWordData()
 
 		val results = stream.foreachRDD(rdd => {
 			val spark = SparkSession.builder.config(rdd.sparkContext.getConf).getOrCreate()
 			import spark.implicits._
 
 			val myTweets = rdd.filter(tweet => tweet.getLang() != null && tweet.getLang() == "en")
-			if (!myTweets.isEmpty)
-				myTweets.map(tweet => {
-					val text  = {
-						if (tweet.getRetweetedStatus() != null)
-							tweet.getRetweetedStatus().getText()
-						else
-							tweet.getText()
-					}
-					text
-				}).map(word2vec.process).collect()
-				println("new tweets: " + myTweets.count)
+			val processed = myTweets.map(tweet => {
+					if (tweet.getRetweetedStatus() != null)
+						tweet.getRetweetedStatus().getText()
+					else
+						tweet.getText()
+				}).map(tweet => Word2vec.process(tweet, wordData))
+			val uniqueWords = processed.flatMap(x => x).distinct().collect()
+			if (uniqueWords.size > 0){
+				Word2vec.getNewMap(wordData, uniqueWords)
+			}
+
+			
+			println(wordData.keys.size)
+			println("new tweets: " + myTweets.count)
 		})
 
+		var old = System.nanoTime
+		while(true){
+			if (System.nanoTime - old > 10*1e9){
+				old = System.nanoTime
+				writeToCassandra(wordData,sc)
+				ssc.stop(false, true)
+				ssc.start()
+			}
+		}
 		ssc.start()
-    	ssc.awaitTermination()
+		ssc.awaitTermination()
+		/*
+		for(a <- 1 to 5){
+			//writeToCassandra(wordData, session)
+			
+			ssc.start()
+    		ssc.awaitTerminationOrTimeout(11 * 1000)
+    		writeToCassandra(wordData, sc)
+    	}
+    	ssc.stop()
+    	*/
+    	session.close()
 	}
 }
--- project/Word2vec.scala
+++ project/Word2vec.scala
@@ -1,3 +1,5 @@
+package Word2vec
+
 import org.apache.spark.sql.cassandra._
 import com.datastax.spark.connector._
 import com.datastax.driver.core.{Session, Cluster, Host, Metadata}
@@ -9,14 +11,16 @@ import org.apache.spark.streaming._
 
 
 
-class Word2vec(conf: SparkConf) extends Serializable {
+object Word2vec {
 	//val spark = SparkSession.builder.config(conf).config("spark.cassandra.connection.host", "127.0.0.1").getOrCreate()
 	//val sc = spark.sparkContext
 	//import spark.implicits._
 	//createTable()
 
-	var wordData = scala.collection.mutable.Map[String, Int]()
-	var mapLength = wordData.keys.size
+	//var wordData = scala.collection.mutable.Map[String, Int]()
+	//var mapLength = wordData.keys.size
+
+
 	
 	/*
 	val df = spark.read.format("org.apache.spark.sql.cassandra").options(Map( "table" -> "words", "keyspace" -> "project" )).load()
@@ -28,19 +32,56 @@ class Word2vec(conf: SparkConf) extends Serializable {
     rdd.saveToCassandra("project", "words", SomeColumns("word", "id", "vector"))
     */
 
-    def process(tweet: String){
+    def process(tweet: String, wordData: scala.collection.mutable.Map[String, Int]) = {
     	var newWord = false
-    	val tokens = tweet.split(" ")
+    	val reTweet = tweet.filter(purge)
+    	val tokens = reTweet.split(" ")
+    	var newWords = scala.collection.mutable.Set[String]()
     	tokens.foreach{x =>
-    		if (!wordData.contains(x)){
-    			wordData(x) = mapLength
-    			mapLength += 1
-    			newWord = true
-    		}
+    		var append = true
+    		val word = x.toLowerCase
+    		if (wordData.contains(word))
+    			append = false
+    		else if (word == "")
+    			append = false
+    		else if(word.startsWith("http"))
+    			append = false
+    		else if(isStopWord(word))
+    			append = false
+    		if(append)
+    			newWords += word
     	}
-    	if (newWord){
-    		println("Unique words: " + mapLength)
-    		//storeWordData()
+    	newWords.toSet
+    }
+
+    def purge(c: Char) = {
+    	val llegals = Set('q','w','e','r','t','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m','`',''',' ','\t')
+    	llegals.contains(c)
+    }
+
+    def isStopWord(word: String) = {
+    	val stopWord = Set("a","about","above","after","again","against","all","am","an","and","any","are","aren't","as","at","be","because","been","before","being","below","between","both","but","by","can't","cannot","could","couldn't","did","didn't","do","does","doesn't","doing","don't","down","during","each","few","for","from","further","had","hadn't","has","hasn't","have","haven't","having","he","he'd","he'll","he's","her","here","here's","hers","herself","him","himself","his","how","how's","i","i'd","i'll","i'm","i've","if","in","into","is","isn't","it","it's","its","itself","let's","me","more","most","mustn't","my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours","ourselves","out","over","own","same","shan't","she","she'd","she'll","she's","should","shouldn't","so","some","such","than","that","that's","the","their","theirs","them","themselves","then","there","there's","these","they","they'd","they'll","they're","they've","this","those","through","to","too","under","until","up","very","was","wasn't","we","we'd","we'll","we're","we've","were","weren't","what","what's","when","when's","where","where's","which","while","who","who's","whom","why","why's","with","won't","would","wouldn't","you","you'd","you'll","you're","you've","your","yours","yourself","yourselves")
+		stopWord.contains(word)    
+    }
+
+    def containsNumber(word: String): Boolean = {
+    	val numbers = Set('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+    	for(c <- word){
+    		if(numbers.contains(c))
+    			return true
+    	}
+    	false
+    }
+
+    def getInitWordData() = {
+    	scala.collection.mutable.Map[String, Int]()
+    }
+
+    def getNewMap(orig: scala.collection.mutable.Map[String, Int],newWords: Array[String]) = {
+    	var value = orig.keys.size
+    	newWords.foreach{x =>
+    		orig(x) = value
+    		value += 1
     	}
     }
 
